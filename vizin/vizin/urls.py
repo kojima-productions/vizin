b119d1478d10313125c042275278f6abdbbc8171
Matheus Vinícius <matheusviolss@gmail.com>
2026-06-03 17:46:26 +0000

    feat: Redirect de '/' para 'administrador/registro'

--- vizin/vizin/urls.py
+++ vizin/vizin/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/administrador/registro/', permanent=False)),
     path('admin/', admin.site.urls),
     path('administrador/', include('apps.administrador.urls')),
 
