de14f09c563bc0b38463446b4f2d31e4abb69592
MatheusViols <matheusviolss@gmail.com>
2026-06-10 13:48:37 +0000

    feat: Criação do model Comunicado

--- vizin/apps/administrador/models.py
+++ vizin/apps/administrador/models.py
@@ -11,3 +11,32 @@ class Administrador(models.Model):
 
     class Meta:
         db_table = "administrador"
+
+class Comunicado(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=1000)
+    data = models.DateTimeField(auto_now_add=True)
+
+    class TipoComunicado(models.TextChoices):
+        MANUTENCAO = "Manutenção"
+        ASSEMBLEIA = "Assembleia"
+        SEGURANCA = "Segurança"
+        EVENTO = "Evento"
+        URGENTE = "Urgente"
+        FINANCEIRO = "Financeiro"
+        CONVIVENCIA = "Convivencia"
+        FALTA_AGUA = "Falta de agua"
+        FALTA_ENERGIA = "Falta de energia"
+
+    tipo = models.CharField(max_length=16, choices=TipoComunicado.choices)
+
+    administrador = models.ForeignKey(Administrador, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Comunicado: {self.titulo} | {self.data}"
+
+    class Meta:
+        db_table = "comunicado"
+
+
+
